0535d768ec4af0f77aa25936c5d0234ddce465b8
DJ Bowers <dj@dj-bowers.com>
2020-04-06 13:52:12 -0700

    add main control logic and prompt for user input

--- src/readingList.js
+++ src/readingList.js
@@ -1,15 +1,39 @@
+const prompt = require('prompt-sync')
+
 const { MAIN_PROMPT, ADD_BOOK_PROMPT } = require('./prompts')
 
 class ReadingList {
   constructor() {
     this.books = []
+    this.prompt = prompt({ sigint: true })
+    this.isOpen = false
   }
 
   open() {
     console.log('\nWelcome to your Reading List, powered by Google Books.')
-    console.log(this.books)
-    console.log(MAIN_PROMPT)
-    console.log(ADD_BOOK_PROMPT)
+    this.isOpen = true
+    this.continue()
+  }
+
+  continue() {
+    while (this.isOpen) {
+      console.log(MAIN_PROMPT)
+      const action = this.prompt('> ').trim()
+      switch (action) {
+        case 'v':
+          console.log('view books')
+          break
+        case 'a':
+          console.log('add book')
+          break
+        case 'q':
+          this.isOpen = false
+          break
+        default:
+          console.log('\nInvalid input, please try again...')
+          break
+      }
+    }
   }
 }
 
