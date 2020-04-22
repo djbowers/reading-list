873317a9b5cbea517bad40544fbc1837b895cfc5
DJ Bowers <dj@dj-bowers.com>
2020-04-21 17:08:07 -0700

    add tests for prompts module
    also add timeout to google books api test

--- test/google-books.test.js
+++ test/google-books.test.js
@@ -5,15 +5,16 @@ const { searchGoogleBooks } = require('../src/google-books')
 describe('Google Books', () => {
   describe('searchGoogleBooks', () => {
     it('should get a valid response from the Google Books API', async () => {
-      const books = await searchGoogleBooks('code')
-      expect(books).to.have.lengthOf(5)
+      const results = 5
+      const books = await searchGoogleBooks('test', results)
 
+      expect(books).to.have.lengthOf(results)
       books.forEach((book) => {
         expect(book).to.haveOwnProperty('title')
         expect(book).to.haveOwnProperty('authors')
         expect(book).to.haveOwnProperty('publisher')
         expect(book).to.haveOwnProperty('display')
       })
-    })
+    }).timeout(10000)
   })
 })
